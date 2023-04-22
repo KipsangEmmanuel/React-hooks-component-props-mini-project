0c97eb0c97a3606f1a94f0620aa2b31eb61157d9
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-04-22 12:40:53 -0700

    added Header component

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+// function About() {
+//     return (
+//     <aside>
+//         <img src="https://m.media-amazon.com/images/I/91-dIy8vdDL._SL1500_.jpg" alt="" />
+//         <p>Personal blog by Dan Abramov.</p>
+//         <p>I explain with words and code</p>
+//     </aside>
+//     );
+
+// };
+// export default About;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Header(props) {
+    return (
+        <Header>
+            <h1>{props.blogName}</h1>
+        </Header>
+    );
+
+};
+export default Header;
